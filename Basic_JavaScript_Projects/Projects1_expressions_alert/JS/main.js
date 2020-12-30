8cde8c5ab3649a6c8341f8596404d842b968f250
DesireeFredenburg <des.fredenburg@gmail.com>
2020-12-29 17:49:35 -0800

    JavaScript Assignment

---
+++ Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+document.write("Learning JavaScript"); // using documment.write to show a string
+var A = "This is a string."; //assigning a string to a variable 
+document.write(A); // printing the variable A
+window.alert(A); // creating a window alert
+document.write("Lisa told Bart,\"Knock it off Bart! Or I will tell Dad!\"<br> \"Eat my shorts Lisa!\"Bart responds.");
+document.write("\"Be who you are and say what you feel, "
++ "because those who mind don\'t matter and those who matter don\'t mind.\" "
++ "-Dr. Seuss"); // learning to escape the character
+var B = "Who would have thunk it." + "This is an attempt at a concatenated string." // concatenating a string and assigning it to a variable
+document.write(B) // printing variable B
+var Family = "The Honeau's", Mom = "Desiree", Son = "Homer", Daughter = "Juneau"; // assigning multiple variables
+var Family = Family.fontcolor("red"); // using str.fontcolor method to change colors
+var Mom = Mom.fontcolor("red");
+var Son = Son.fontcolor("red");
+var Daughter = Daughter.fontcolor("red");
+document.write(Family); //printing variable Family
+var blues = "I have the blues."; // assigning string to variable
+var blues = blues.fontcolor("blue"); //assigning font color to variable
+document.write(blues); // printing variable 
+document.write(3+3); // writting an expression - this is a statement containing an expression
